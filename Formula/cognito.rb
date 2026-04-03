a960be4b41dfa3b95db76b621a40aeceed9b41af
romus <688950+romus@users.noreply.github.com>
2026-04-03 08:16:24 +0000

    chore: update cognito to v0.1.1

--- Formula/cognito.rb
+++ Formula/cognito.rb
@@ -3,8 +3,8 @@ class Cognito < Formula
 
   desc "CLI tool for anonymizing and restoring project-specific paths and words"
   homepage "https://github.com/romus/cognito"
-  url "https://github.com/romus/cognito/releases/download/v0.1.0/cognito-0.1.0.tar.gz"
-  sha256 "b14ab6be49ce283525149c1f4d27b0ee96833dc7edb0d5f9fc7e10bf97ab00ab"
+  url "https://github.com/romus/cognito/releases/download/v0.1.1/cognito-0.1.1.tar.gz"
+  sha256 "957efd0f3606ad2ca5f584a11259ad7d7dcb20cfc807c4a50c7abffdfaa70ff6"
 
   depends_on "python@3.14"
 
@@ -13,7 +13,7 @@ class Cognito < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/cognito --version")
+    assert_match "0.1.1", shell_output("#{bin}/cognito --version")
     config = testpath/"config.json"
     system bin/"cognito", "init-config", "--config", config
     assert_predicate config, :exist?
